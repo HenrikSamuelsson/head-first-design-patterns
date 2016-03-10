66fd826224e4035bda9f9205c41e1d579f22a109
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2016-03-10 17:46:12 +0100

    Adds code for sharpen your pencil exercise.

---
+++ exercises/workspace/Chapter 05 Page 178 - Sharpen your pencil/src/ChocolateBoiler.java
@@ -0,0 +1,28 @@
+
+public class ChocolateBoiler {
+	private boolean empty;
+	private boolean boiled;
+	private ChocolateBoiler singletonInstance;
+	
+	private ChocolateBoiler() {
+		empty = true;
+		boiled = false;
+	}
+	
+	public ChocolateBoiler getInstance () {
+		if (singletonInstance == null) {
+			singletonInstance = new ChocolateBoiler();
+		}
+		return singletonInstance;
+	}
+	
+	public void fill() {
+		if (isEmpty()) {
+			empty = false;
+			boiled = false;
+			// fill the boiler with a milk/chocolate mixture
+		}
+	}
+	
+	// rest of ChocolateBoiler code...
+}
